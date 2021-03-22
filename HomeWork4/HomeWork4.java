380c7850fc41a217c585a087e6aea9d22d2c0801
Alexander Chekhov <chekhovsasha4737@gmail.com>
2021-03-22 17:58:58 +0800

    Updated HomeWork4

--- HomeWork4/HomeWork4.java
+++ HomeWork4/HomeWork4.java
@@ -1,8 +1,7 @@
 package com.company;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,9 +15,8 @@ public class HomeWork4 {
      * Main method with homework
      * @param args Arguments from command line (terminal)
      * @throws IllegalArgumentException When first argument wasn't received
-     * @throws IOException If file is not found or something about it
      */
-    public static void main(String[] args) throws IllegalArgumentException, IOException {
+    public static void main(String[] args) throws IllegalArgumentException {
         // Check for first argument in command line is exists
         try {
             String temp = args[0];
@@ -26,21 +24,29 @@ public class HomeWork4 {
             throw new IllegalArgumentException("Argument wasn't received.");
         }
         // Scanner for reading from file
-        Scanner in = new Scanner(new FileReader(args[0]));
+        Scanner in = null;
+        try {
+             in = new Scanner(new FileInputStream(args[0]), "windows-1251");
+        } catch (IOException ex) {
+            System.out.println("Путь к файлу содержит ошибку.");
+        }
         // Pattern for searching words in file
-        Pattern pattern = Pattern.compile("[A-Za-z]+");
+        Pattern pattern = Pattern.compile("[A-Za-zА-Яа-я]+");
         // Variable for answer
         int answer = 0;
-        // Iterate for lines in file
-        while (in.hasNextLine()) {
-            // Create matcher for searching words in file by pattern
-            Matcher matcher = pattern.matcher(in.nextLine());
-            // Iterate for found words
-            while (matcher.find()) {
-                // Increment the answer variable for every word
-                answer++;
+
+        if (in != null)
+            // Iterate for lines in file
+            while (in.hasNextLine()) {
+                // Create matcher for searching words in file by pattern
+                Matcher matcher = pattern.matcher(in.nextLine());
+                // Iterate for found words
+                while (matcher.find()) {
+                    // Increment the answer variable for every word
+                    answer++;
+                }
             }
-        }
+
         // Print answer
         System.out.println(answer);
     }
